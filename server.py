3a5003c1e21d84db31976dd49dd57635ddef12da
ed system on behalf of Tyson Thomas <ed>
2021-10-20 02:41:52 +1100

    Scaffold update by instructor

---
+++ server.py
@@ -0,0 +1,30 @@
+#!/bin/python
+import signal
+import os
+import sys
+import socket
+
+
+#Use this variable for your loop
+daemon_quit = False
+
+#Do not modify or remove this handler
+def quit_gracefully(signum, frame):
+    global daemon_quit
+    daemon_quit = True
+
+
+
+def run():
+    #Do not modify or remove this function call
+    signal.signal(signal.SIGINT, quit_gracefully)
+
+    # Call your own functions from within 
+    # the run() funcion
+    pass
+
+
+if __name__ == '__main__':
+    run()
+
+
